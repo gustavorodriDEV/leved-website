652fc172ebcc748f05c601969984b665fb97a670
Gustavo Rodrigues <gustavorodriguesoliveira012@gmail.com>
2025-08-13 14:43:40 -0300

    refactor(navbar): Substitui menu de colapso por Off-canvas responsivo

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,35 +1,88 @@
-import { Navbar, Nav, Container } from "react-bootstrap";
+import React, { useState } from "react";
+import { Navbar, Nav, Container, Offcanvas } from "react-bootstrap";
+import { Link } from "react-scroll";
 import styles from "./Navbar.module.css";
 
 const LevedNavbar = () => {
+  const [showOffcanvas, setShowOffcanvas] = useState(false);
+
+  const handleClose = () => setShowOffcanvas(false);
+  const handleShow = () => setShowOffcanvas(true);
+
+  const navLinks = [
+    { to: "home", text: "Home" },
+    { to: "sobre-nos", text: "Sobre Nós" },
+    { to: "produtos", text: "Produtos" },
+    { to: "servicos", text: "Serviços" },
+    { to: "contato", text: "Contato" },
+  ];
+
   return (
-    <Navbar expand="lg" className={styles.navbarWrapper} fixed="top" variant="dark">
-      <Container>
-        <Navbar.Brand href="#home" className={styles.logo}>
-          Leved Origin
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="ms-auto">
-            <Nav.Link href="#home" className={styles.navLink}>
-              Home
-            </Nav.Link>
-            <Nav.Link href="#sobre-nos" className={styles.navLink}>
-              Sobre nós
-            </Nav.Link>
-            <Nav.Link href="#produtos" className={styles.navLink}>
-              Produtos
-            </Nav.Link>
-            <Nav.Link href="#servicos" className={styles.navLink}>
-              Serviços
-            </Nav.Link>
-            <Nav.Link href="#contato" className={styles.navLink}>
-              Contato
-            </Nav.Link>
+    <>
+      <Navbar
+        expand="lg"
+        className={styles.navbarWrapper}
+        fixed="top"
+        variant="dark"
+      >
+        <Container className="pe-1">
+          <Navbar.Brand href="#home" className={styles.logo}>
+            Leved Origin
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="offcanvasNavbar" onClick={handleShow} />
+          <Navbar.Collapse
+            id="basic-navbar-nav"
+            className="d-none d-lg-flex justify-content-end"
+          >
+            <Nav>
+              {navLinks.map((link) => (
+                <Link
+                  key={link.to}
+                  to={link.to}
+                  smooth={true}
+                  duration={500}
+                  offset={-70}
+                  className={styles.navLink}
+                >
+                  {link.text}
+                </Link>
+              ))}
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+
+      <Navbar.Offcanvas
+        id="offcanvasNavbar"
+        show={showOffcanvas}
+        onHide={handleClose}
+        placement="end"
+        className={styles.offcanvasMenu}
+      >
+        <Offcanvas.Header closeButton>
+          <Offcanvas.Title className={styles.offcanvasTitle}>
+            Menu
+          </Offcanvas.Title>
+        </Offcanvas.Header>
+        <Offcanvas.Body>
+          <Nav className="justify-content-end flex-grow-1 pe-3 flex-column">
+            {navLinks.map((link) => (
+              <Link
+                key={link.to}
+                to={link.to}
+                smooth={true}
+                duration={500}
+                offset={-70}
+                className={styles.navLink}
+                onClick={handleClose}
+              >
+                {link.text}
+              </Link>
+            ))}
           </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+        </Offcanvas.Body>
+      </Navbar.Offcanvas>
+    </>
   );
 };
 
